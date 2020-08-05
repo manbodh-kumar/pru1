e489d14f8d1516eb8e0a86b55ca5c20f5e87cc4f
Mandy <mandy@33gmail.com>
2020-08-05 12:37:09 +0530

    First commit

---
+++ src/Module/AdvancedSearch/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const AdvancedSearch=()=>{
+    return (
+        <h1>Advanced Search</h1>
+    );
+};
+export default AdvancedSearch;
---
+++ src/Module/AnnualReports/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const AnnualReports = () => {
+    return (
+        <h1>Annual Reports</h1>
+    );
+};
+
+export default AnnualReports;
